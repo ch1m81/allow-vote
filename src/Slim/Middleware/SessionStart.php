a0c926fe9c80f11bde5a31aeb9255035f7c8e9d5
ctk <ch1m81@gmail.com>
2016-09-23 10:27:33 +0200

    Update SessionStart.php

--- src/Slim/Middleware/SessionStart.php
+++ src/Slim/Middleware/SessionStart.php
@@ -18,26 +18,26 @@ class SessionStart
      *
      * @param array $settings
      */
-    public function __construct($settings = [])
-    {
+	public function __construct($settings = [])   {
         				
-				$defaults = [
-            'lifetime'    => '30 minutes',
-            'path'        => '/',
-            'domain'      => null,
-            'secure'      => false,
-            'httponly'    => false,
-            'name'        => 'supportfaq',
-            'autorefresh' => true,
-        ];
+	$defaults = [
+        'lifetime'    => '30 minutes',
+        'path'        => '/',
+        'domain'      => null,
+        'secure'      => false,
+        'httponly'    => false,
+        'name'        => 'supportfaq',
+        'autorefresh' => true,
+    ];
 				
-        $settings = array_merge($defaults, $settings);
+    $settings = array_merge($defaults, $settings);
 
-        if (is_string($lifetime = $settings['lifetime'])) {
-            $settings['lifetime'] = strtotime($lifetime) - time();
-        }				
+    if (is_string($lifetime = $settings['lifetime'])) {
+        $settings['lifetime'] = strtotime($lifetime) - time();
+    }				
 				
-        $this->settings = $settings;
+    $this->settings = $settings;
+	$this->isCapthaValid = Array("valid"=>false);
 
         ini_set('session.gc_probability', 0);
         ini_set('session.gc_divisor', 1);
@@ -53,66 +53,76 @@ class SessionStart
      *
      * @return \Psr\Http\Message\ResponseInterface
      */
-    public function __invoke(Request $request, Response $response, callable $next)
-    {
+    public function __invoke(Request $request, Response $response, callable $next) {
 			
-			// get the session
-			session_name($this->settings["name"]); 
-			session_start();	
-			
-			$isCapthaValid = Array("valid"=>false);
-			
-			$noOfAttempts = $this->getNumberOfAttempt();
-			
-			// there is more then 3 failed attempts, activate captcha
-			if ($noOfAttempts > 3) {
-				
-				// expecting header to have "gCaptcha" filed with values from the client-verification				
-				if ($request->hasHeader('gCaptcha') && $request->getHeaderLine('gCaptcha') !== "false") {
-						
-						$isCapthaValid = $this->tryCaptcha($request->getHeaderLine('gCaptcha'));						
-				} else {			
-
-					$attemptNo = $this->increaseLoginAttempt();					
-					
-					// captcha is expeced not it is missing
-					return $response->withHeader('Attempt', $attemptNo)->withJson("The captcha is missing, please try again", 422);
-				}
-			}
-			
-			
-			if ($isCapthaValid["valid"] || $noOfAttempts < 4) {
-				// call MAIN FUNCTION
-				$response = $next($request, $response); 
-
-				// returns code of main function, 200 ||403 ... 
-				$statusCode = $response->getStatusCode();			
-				
-				// "OK" if ok
-				$statusMessage = (!empty($response->getHeader('status'))) ? $response->getHeader('status')[0] : false;				
-				
-				if ($statusCode === 200 && $statusMessage === "OK") {
-					
-					$this->startSession();					
-				
-				} else {				
-					
-					$attemptNo = $this->increaseLoginAttempt();	
-					
-					return $response->withHeader('Attempt', $attemptNo);
-				}
+		// set the "login" session & start a session
+		session_name("login"); 
+		session_start();	
+	
+		// per default captcha is not valid
+		$isCapthaValid = Array("valid"=>false);
 		
-				return $response;		
+		// get number of attepts sofar (out of $_SESSION)
+		$noOfAttempts = $this->getNumberOfAttempt();
+		
+		// there is more then 3 failed attempts, activate captcha
+		if ($noOfAttempts > 3) {
+		
+			// expecting header to have "gCaptcha" filed with values from the client-verification				
+			if ($request->hasHeader('gCaptcha') && $request->getHeaderLine('gCaptcha') !== "false") {
+					
+					// validate captcha - server side
+					$this->isCapthaValid = $this->tryCaptcha($request->getHeaderLine('gCaptcha'));						
+			} else {			
+		
+				// log attept + 1
+				$attemptNo = $this->increaseLoginAttempt();					
 				
-			} else {
+				// pass response + N° of attepts to the client 
+				return $response->withHeader('Attempt', $attemptNo)->withJson("The captcha is missing, please try again", 422);
+			}
+		}
+	
+		// captcha = OK || no need for captcha
+		if ($this->isCapthaValid["valid"] || $noOfAttempts < 4) {
+			
+			// call MAIN FUNCTION
+			$response = $next($request, $response); 
+		
+			// returns code of main function, 200 ||403 ... 
+			$statusCode = $response->getStatusCode();			
+			
+			// "OK" if ok
+			$statusMessage = (!empty($response->getHeader('status'))) ? $response->getHeader('status')[0] : false;				
+			
+			// if the auth passed start session
+			if ($statusCode === 200 && $statusMessage === "OK") {
 				
+				// start session
+				$this->startSession();					
+			
+			// auth failed, log attempt 
+			} else {				
+				
+				// log attept + 1
 				$attemptNo = $this->increaseLoginAttempt();	
 				
-				return $response->withJson("The captcha is not valid, please try again", 422 )->withHeader('Attempt', $attemptNo);
+				// pass response + N° of attepts to the client 
+				return $response->withHeader('Attempt', $attemptNo);
 			}
+		
+			// return
+			return $response;		
 			
+		// captca is invalid 
+		} else {
 			
+			// log attept + 1
+			$attemptNo = $this->increaseLoginAttempt();	
 			
+			// pass response + N° of attepts to the client 
+			return $response->withJson("The captcha is not valid, please try again", 422 )->withHeader('Attempt', $attemptNo);
+		}
     }
 
     /**
@@ -121,54 +131,57 @@ class SessionStart
      */
     protected function increaseLoginAttempt()  { 
 					
-			if(!empty($_SESSION)){
-				if (isset($_SESSION['attemptNo'])) {
-					$_SESSION['attemptNo']++;
-				} else {
-					$_SESSION['attemptNo'] = 1;
-				}
-							
-			}else{
+		if(!empty($_SESSION)){
+			if (isset($_SESSION['attemptNo'])) {
+				$_SESSION['attemptNo']++;
+			} else {
 				$_SESSION['attemptNo'] = 1;
-			};
-			
-			return $_SESSION['attemptNo'];
+			}
+						
+		}else{
+			$_SESSION['attemptNo'] = 1;
+		};
+		
+		return $_SESSION['attemptNo'];
     }
 		
-		/**
-		// validate captcha server side
-		// returns Array(status, error message)
-		*/
-		protected function tryCaptcha($clientHash)  { 					
-			
-			$recaptcha = new \ReCaptcha\ReCaptcha("6LcYMQcUAAAAANa7cTGOpIxhYAR32BH92HEFYYB4");
-			$resp = $recaptcha->verify($clientHash);
-			
-			// captcha ok? proceed with auth
-			return ($resp->isSuccess()) ? Array("valid" => true, "message" => "all ok") : Array("valid" => false, "message" => $resp->getErrorCodes());						
+		
+	/**
+	// validate captcha server side
+	// returns Array(status, error message)
+	*/
+	protected function tryCaptcha($clientHash)  { 					
+		
+		$recaptcha = new \ReCaptcha\ReCaptcha("6LcYMQcUAAAAANa7cTGOpIxhYAR32BH92HEFYYB4");
+		$resp = $recaptcha->verify($clientHash);
+		
+		// captcha ok? proceed with auth
+		return ($resp->isSuccess()) ? Array("valid" => true, "message" => "all ok") : Array("valid" => false, "message" => $resp->getErrorCodes());						
+	}
+		
+		
+	/**
+	// returns number of attempts INT
+	*/
+	protected function getNumberOfAttempt()  { 
+				
+		if(!empty($_SESSION)){				
+			if (isset($_SESSION['attemptNo'])) {					
+				return $_SESSION['attemptNo'];					
+			} 							
 		}
 		
+		$_SESSION['attemptNo'] = 1;				
+		return $_SESSION['attemptNo'];
+	 }
+		
 		/**
-		// returns number of attempts INT
+		// destroy current session and start new one
 		*/
-		protected function getNumberOfAttempt()  { 
-					
-			if(!empty($_SESSION)){				
-				if (isset($_SESSION['attemptNo'])) {					
-					return $_SESSION['attemptNo'];					
-				} 							
-			}
-			
-			$_SESSION['attemptNo'] = 1;				
-			return $_SESSION['attemptNo'];
-    }
-		
-		
-		protected function startSession()
-    {
+	protected function startSession()   {
         $settings = $this->settings;
         $name = $settings['name'];
-
+	
         session_set_cookie_params(
             $settings['lifetime'],
             $settings['path'],
@@ -176,28 +189,16 @@ class SessionStart
             $settings['secure'],
             $settings['httponly']
         );
-
-        /*if (session_id()) {
-            if ($settings['autorefresh'] && isset($_COOKIE[$name])) {
-                setcookie(
-                    $name,
-                    $_COOKIE[$name],
-                    time() + $settings['lifetime'],
-                    $settings['path'],
-                    $settings['domain'],
-                    $settings['secure'],
-                    $settings['httponly']
-                );
-            }
-        }*/
-				
-				//session_destroy();
-				//$_SESSION = array();
-        //session_name($name);
-        //session_cache_limiter(false);
-				//session_start();
-				session_regenerate_id( true );
-				$_SESSION['attemptNo'] = 0;
-				$_SESSION['token'] = session_id();
-    }
+					
+		setcookie("login", "", time()-3600);				
+		unset($_COOKIE["login"]);
+		session_destroy();
+	       
+		session_name($name);
+        session_cache_limiter(false);
+		session_start();
+					
+		$_SESSION = array();			
+		$_SESSION['token'] = session_id();
+    	}
 }
