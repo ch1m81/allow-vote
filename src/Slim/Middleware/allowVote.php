a749f2ffc4462cd5d2a5fc77f43567f35d0a66a5
ctk <ch1m81@gmail.com>
2017-01-16 11:04:32 +0100

    Update and rename hasVoted.php to allowVote.php

--- src/Slim/Middleware/allowVote.php
+++ src/Slim/Middleware/allowVote.php
@@ -6,7 +6,7 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 use \Exception;
 
-class IsLoggedIn
+class allowVote
 {
     /**
      * @var array
